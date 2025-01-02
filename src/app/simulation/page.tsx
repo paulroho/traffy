fe4b68e7c73995436bab1feaf2b3b5d84f2aa7a4
Paul Rohorzka <paul.rohorzka@squer.io>
2025-01-02 14:43:37 +0100

    some funnily rotating text

--- src/app/simulation/page.tsx
+++ src/app/simulation/page.tsx
@@ -4,13 +4,34 @@ import Canvas from "@/components/Canvas";
 
 export default function Simulation() {
   const draw = (ctx: CanvasRenderingContext2D, frameCount: number) => {
+    const center = {
+      x: ctx.canvas.width / 2,
+      y: ctx.canvas.height / 2
+    };
+
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     ctx.fillStyle = '#ff3456';
     ctx.fillRect(10, 10, ctx.canvas.width - 20, ctx.canvas.height - 20);
 
     ctx.font = "30px sans-serif";
+
+    ctx.save();
+
+    ctx.translate(center.x, center.y);
+    ctx.rotate(2 * Math.PI * frameCount / 400);
+
     ctx.fillStyle = "white";
-    ctx.fillText(ctx.canvas.width + 'x' + ctx.canvas.height, 10, 40);
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText(ctx.canvas.width + 'x' + ctx.canvas.height, 0, 0);
+
+    ctx.restore();
+
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.arc(center.x, center.y, 4, 0, 2 * Math.PI);
+    ctx.fill();
+
     ctx.textAlign = "right";
     ctx.strokeText(frameCount + '', ctx.canvas.width - 10, 40);
   }
