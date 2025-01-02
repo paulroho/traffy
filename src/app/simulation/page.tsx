fddf60d5108d31af96a0e1162ac1fdf9bf2d32af
Paul Rohorzka <paul.rohorzka@squer.io>
2025-01-02 11:16:27 +0100

    add component Visualization

---
+++ src/app/simulation/page.tsx
@@ -0,0 +1,11 @@
+import Visualization from "@/components/Visualization";
+
+export default function Simulation() {
+  return (
+    <div className="h-lvh p-4">
+      <main className="flex flex-col items-center h-full">
+        <Visualization/>
+      </main>
+    </div>
+  );
+}
---
+++ src/components/Visualization.tsx
@@ -0,0 +1,5 @@
+export default function Visualization() {
+    return (
+        <canvas id="visualization" className="w-full h-full border-red-300 border-2"></canvas>
+    );
+}
