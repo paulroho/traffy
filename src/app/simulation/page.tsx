35dd4db17afa9cb8c51c7cd3d06631b05c30516e
Paul Rohorzka <paul.rohorzka@squer.io>
2025-01-02 16:55:05 +0100

    add simple grid

--- src/app/simulation/page.tsx
+++ src/app/simulation/page.tsx
@@ -3,13 +3,28 @@
 import Canvas from "@/components/Canvas";
 import { clock } from "@/visualization/components/clock";
 import { debugInfo } from "@/visualization/components/debugInfo";
+import grid from "@/visualization/components/grid";
 import { someBackground } from "@/visualization/components/someBackground";
 
 export default function Simulation() {
   const draw = (ctx: CanvasRenderingContext2D, frameCount: number) => {
+    const aGrid = grid({
+      x: {
+        start: 0,
+        end: 10000,
+        interval: 500,
+      },
+      y: {
+        start: -1000,
+        end: +1000,
+        interval: 100,
+      }
+    });
+
     const graphic = [
       someBackground(),
       debugInfo(),
+      aGrid,
       clock(),
     ];
 
---
+++ src/visualization/components/grid.ts
@@ -0,0 +1,37 @@
+import { Renderable } from "../Renderable";
+
+type GridOptionsDimension = {
+    start: number,
+    end: number,
+    interval: number,
+};
+type GridOptions = {
+    x: GridOptionsDimension,
+    y: GridOptionsDimension,
+};
+
+export default function grid(options: GridOptions): Renderable {
+    function* positions(options: GridOptionsDimension) {
+        for (let pos = options.start; pos <= options.end; pos += options.interval) {
+            yield pos;
+        }
+    };
+
+    return {
+        render: ctx => {
+            ctx.beginPath();
+
+            for (const x of positions(options.x)) {
+                ctx.moveTo(x, options.y.start);
+                ctx.lineTo(x, options.y.end);
+            }
+
+            for (const y of positions(options.y)) {
+                ctx.moveTo(options.x.start, y);
+                ctx.lineTo(options.x.end, y);
+            }
+
+            ctx.stroke();
+        }
+    }
+}
