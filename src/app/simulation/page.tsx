52eda0316fa7e7270756e4edbb75f58b5cc73114
Paul Rohorzka <paul.rohorzka@squer.io>
2025-01-02 14:56:49 +0100

    add funny second hand

--- src/app/simulation/page.tsx
+++ src/app/simulation/page.tsx
@@ -14,17 +14,34 @@ export default function Simulation() {
     ctx.fillRect(10, 10, ctx.canvas.width - 20, ctx.canvas.height - 20);
 
     ctx.font = "30px sans-serif";
+    ctx.textBaseline = "alphabetic";
 
     ctx.save();
 
     ctx.translate(center.x, center.y);
-    ctx.rotate(2 * Math.PI * frameCount / 400);
+    
+    const now = new Date();
+
+    ctx.rotate(2 * Math.PI * (now.getSeconds() + now.getMilliseconds()/1000)/60);
+
+    ctx.lineWidth = 5;
+    ctx.lineCap = "round"
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(0, -100);
+    ctx.stroke();
 
     ctx.fillStyle = "white";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
-    ctx.fillText(ctx.canvas.width + 'x' + ctx.canvas.height, 0, 0);
+    ctx.fillText(now.toLocaleTimeString(), 0, 0);
 
+    ctx.restore();
+
+    ctx.save();
+    ctx.fillStyle = "white";
+    ctx.textAlign = "left";
+    ctx.fillText(ctx.canvas.width + 'x' + ctx.canvas.height, 10, 40);
     ctx.restore();
 
     ctx.fillStyle = "black";
