6a5d8d1f028b385590dc3c43e71e28afdd1b0841
Paul Rohorzka <paul.rohorzka@squer.io>
2025-01-05 15:00:33 +0100

    refactor clock and move it to bottom

--- src/visualization/components/dev-tools/clock.ts
+++ src/visualization/components/dev-tools/clock.ts
@@ -1,34 +1,26 @@
 import { Renderable } from "../../basics";
 
 export default function clock(): Renderable {
+  const secondsHandLength = 100;
+
   return {
     render: ctx => {
       const center = {
         x: ctx.canvas.width / 2,
-        y: ctx.canvas.height / 2
+        y: ctx.canvas.height - secondsHandLength
       };
 
       ctx.save();
 
-      ctx.font = "2rem sans-serif";
       ctx.translate(center.x, center.y);
 
       const now = new Date();
 
-      ctx.rotate(2 * Math.PI * (now.getSeconds() + now.getMilliseconds() / 1000) / 60);
+      const seconds = now.getSeconds() + now.getMilliseconds() / 1000;
+      ctx.rotate(2 * Math.PI * seconds / 60);
 
-      ctx.strokeStyle = "yellow";
-      ctx.lineWidth = 5;
-      ctx.lineCap = "round";
-      ctx.beginPath();
-      ctx.moveTo(0, 0);
-      ctx.lineTo(0, -100);
-      ctx.stroke();
-
-      ctx.fillStyle = "white";
-      ctx.textAlign = "center";
-      ctx.textBaseline = "middle";
-      ctx.fillText(now.toLocaleTimeString(), 0, 0);
+      drawSecondsHand(ctx);
+      drawTime(ctx, now);
 
       ctx.restore();
 
@@ -38,4 +30,22 @@ export default function clock(): Renderable {
       ctx.fill();
     }
   };
+
+  function drawSecondsHand(ctx: CanvasRenderingContext2D) {
+    ctx.strokeStyle = "yellow";
+    ctx.lineWidth = 5;
+    ctx.lineCap = "round";
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(0, -secondsHandLength);
+    ctx.stroke();
+  }
+
+  function drawTime(ctx: CanvasRenderingContext2D, now: Date) {
+    ctx.font = "2rem sans-serif";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText(now.toLocaleTimeString(), 0, 0);
+  }
 }
