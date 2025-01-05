f7de81b9f1e08eca2d302c4115d1d2977bd0f020
Paul Rohorzka <paul.rohorzka@squer.io>
2025-01-05 14:08:40 +0100

    use darker green

--- src/visualization/components/dev-tools/someBackground.ts
+++ src/visualization/components/dev-tools/someBackground.ts
@@ -3,7 +3,7 @@ import { Renderable } from "../../basics";
 export default function someBackground(): Renderable {
   return {
     render: ctx => {
-      ctx.fillStyle = '#12dd34';
+      ctx.fillStyle = '#128834';
       ctx.fillRect(10, 10, ctx.canvas.width - 20, ctx.canvas.height - 20);
     }
   };
