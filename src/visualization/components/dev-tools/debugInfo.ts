cd17b794dacdf6ee26688aa10dd51cb5e3b8cdd9
Paul Rohorzka <paul.rohorzka@squer.io>
2025-01-17 16:22:26 +0100

    show world size on top

--- src/visualization/components/dev-tools/debugInfo.ts
+++ src/visualization/components/dev-tools/debugInfo.ts
@@ -8,15 +8,15 @@ export default function debugInfo(): Renderable {
 
       ctx.font = "1.5rem sans-serif";
       ctx.strokeStyle = "white";
-      ctx.textBaseline = "top";
-
-      ctx.fillStyle = "#aaa";
       ctx.textAlign = "left";
-      ctx.fillText(ctx.canvas.width + ' x ' + ctx.canvas.height, 8, 8);
+      ctx.textBaseline = "top";
 
       const visibleWorldRectangle: WorldRectangle = mapper.getVisibleWorldRectangle(ctx.canvas);
       ctx.fillStyle = "white";
-      ctx.fillText(visibleWorldRectangle.extent.east.toFixed(0) + 'm x ' + visibleWorldRectangle.extent.north.toFixed(0) + 'm', 8, 40);
+      ctx.fillText(visibleWorldRectangle.extent.east.toFixed(0) + 'm x ' + visibleWorldRectangle.extent.north.toFixed(0) + 'm', 8, 8);
+
+      ctx.fillStyle = "#aaa";
+      ctx.fillText(ctx.canvas.width + ' x ' + ctx.canvas.height, 8, 40);
 
       ctx.textAlign = "right";
       ctx.strokeText(frameCount + '', ctx.canvas.width - 8, 8);
